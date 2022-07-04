1169af606fa8f62220a5f5f0c8159b0e911495f8
Jakob <jakob.hjortshoj@noaignite.com>
2022-07-04 09:55:09 +0200

    Project setup done

---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Jakob Hjortshøj on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
